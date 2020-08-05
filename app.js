d8e06e84a5fffc09ef1b1867dfabf99c16ce1996
priyankahpheggar <69188044+priyankahpheggar@users.noreply.github.com>
2020-08-05 16:15:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 var home_page = "<h1>Welcome to Priyanka Fashions"
-var contact_page ="<html>
-<body>
-<h1>Contact Vishal Kumar</h1>
-</body>
-</html>"
+var contact_page ="<html><body><h1>Contact Vishal Kumar</h1></body></html>"
 
 //home page
 app.get('/', (req, res) => res.send(home_page))
