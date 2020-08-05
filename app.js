2e5db796e225d5cafb88eed7d40e87ed1ab03b92
priyankahpheggar <69188044+priyankahpheggar@users.noreply.github.com>
2020-08-05 16:13:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express()
 const port = process.env.PORT || 8080
 
 var home_page = "<h1>Welcome to Priyanka Fashions"
+var contact_page ="<html>
+<body>
+<h1>Contact Vishal Kumar</h1>
+</body>
+</html>"
 
 //home page
 app.get('/', (req, res) => res.send(home_page))
+
+//contact page
+app.get('/contact/', (req, res) => res.send(contact_page))
 
 app.listen(port, () => {
   console.log(`my new expresso app is running here http://localhost:${port}`)
